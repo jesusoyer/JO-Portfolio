ac6f227f8a742c22b1608f3a2aa1f0a532a89387
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-11 13:13:19 -0500

    removed starter files from project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the first change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>The begining of the project</h1>
+        
     </div>
   );
 }
