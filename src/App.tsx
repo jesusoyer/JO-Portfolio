3e90e2adbaf38eacdc5cb3089babf8cec18dbbf8
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-11 13:18:03 -0500

    heading and statement

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>The begining of the project</h1>
-        
+        <p>"A web developer with a specialization in creating successful front-end websites and web applications.</p>
     </div>
   );
 }
