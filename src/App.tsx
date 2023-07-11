e458c78ba2f6f3fb868f675f466b78c8970fe2ac
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-11 14:06:55 -0500

    added hello its me

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>The begining of the project</h1>
+        <h2>Hello its me</h2>
         <p>"I am a full stack web developer who creates web applications using React.js, TypeScript, JavaScript, MongoDB, Express.js, and Node.js. I have utilized these technologies to build responsive web applications that enhance the user experience."</p>
     </div>
   );
