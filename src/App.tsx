5abe83c8a40c52646a2a01bcd23d524e69184924
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-12 10:55:07 -0500

    test paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <h1>The begining of the project</h1>
         <h2>Hello its me</h2>
         <p>"I am a full stack web developer who creates web applications using React.js, TypeScript, JavaScript, MongoDB, Express.js, and Node.js. I have utilized these technologies to build responsive web applications that enhance the user experience."</p>
+        <p>adding another paragraph to test</p>
     </div>
   );
 }
