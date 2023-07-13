779a836ded83349e26455545a2d113258cc19229
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-13 15:57:06 -0500

    fixed paragraph in the about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -10,7 +10,7 @@ const { skills } = aboutMeCss;
       <div className="col-span-12  w-screen h-[500px] p-2">
             <h1 className=" mx-auto mb-14 font-semibold text-4xl font-bold mb-28">ABOUT ME</h1>
             <p className="text-center w-8/12 mx-auto text-gray-500">
-            As a Frontend Web Developer, I specialize in building the frontend of websites and web applications. You can see projects I have worked on in the projects section.<br></br>I am actively seeking new job opportunities that challenge me to expand my skills and knowledge. If you have an opportunity that matches my experience and interests, please feel free to contact me.
+            As a Frontend Web Developer, I specialize in building the frontend of websites and web applications. You can see projects I have worked on in the projects section.I am actively seeking new job opportunities that challenge me to expand my skills and knowledge. If you have an opportunity that matches my experience and interests, please feel free to contact me.
             </p>
             <div className="flex justify-between mx-72 mt-4">
               <div className={skills}>HTML</div>
